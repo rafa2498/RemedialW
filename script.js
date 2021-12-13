eb734ecea9ee681f84ff90c1ba9f0fe85a2a2853
rafa2498 <rafatoledo2498@gmail.com>
2021-12-13 16:48:52 -0600

    primerproyecto

---
+++ script.js
@@ -0,0 +1,13 @@
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('./sw.js')
+    .then(reg => console.log('Registro de SW exitoso', reg))
+    .catch(err => console.warn('Error al tratar de registrar el sw', err))
+}
+
+$(function () {
+  $('.bxslider').bxSlider({
+    mode: 'fade',
+    captions: true,
+    slideWidth: 1200
+  });
+});
